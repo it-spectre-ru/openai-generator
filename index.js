b0e98a9c8b0a81e4b2eefb2144849aee81b7e6da
stts <it.spectre.ru@gmail.com>
2023-05-14 22:23:37 +0500

    Building an OpenAI fetch request

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const apiKey = "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"
 
-const url = "https://api.openai.com/v1/completions"
+const url = "https://api.openai.com/v1/completions" 
+
+fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer: ${apiKey}`
+    },
+    body: JSON.stringify({
+        'model': 'text-davinci-003',
+        'prompt': 'What is the capital of Spain?'
+    })
+}).then(response => response.json()).then(data => console.log(data))

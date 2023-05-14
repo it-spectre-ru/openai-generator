5092c38a0bddf642bfe2d7b6925e73474d6e1c44
stts <it.spectre.ru@gmail.com>
2023-05-14 22:46:29 +0500

    The first AI fetch request

--- index.js
+++ index.js
@@ -6,9 +6,25 @@ const apiKey = "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"
 const url = 'https://api.openai.com/v1/completions'
 
 document.getElementById("send-btn").addEventListener("click", () => {
-  if (setupTextarea.value) {
+  // if (setupTextarea.value) {
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
-  }
+  // }
+  fetchBotReply()
 })
 
+function fetchBotReply(){
+  fetch(url,{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      'model': 'text-davinci-003',
+      'prompt': 'Sound enthusiastic in five words or less.'
+    })
+  }).then(response => response.json()).then(data => 
+    movieBossText.innerText = data.choices[0].text
+  )
+}

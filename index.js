8510ed957ec27c447fa598aba87e026236cd985a
stts <it.spectre.ru@gmail.com>
2023-05-14 22:31:08 +0500

    The first AI fetch request

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+const setupTextarea = document.getElementById('setup-textarea') 
+const setupInputContainer = document.getElementById('setup-input-container')
+const movieBossText = document.getElementById('movie-boss-text')
+
 const apiKey = "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"
+const url = 'https://api.openai.com/v1/completions'
 
-const url = "https://api.openai.com/v1/completions" 
+document.getElementById("send-btn").addEventListener("click", () => {
+  if (setupTextarea.value) {
+    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
+    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
+  }
+})
 
-fetch(url, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-    },
-    body: JSON.stringify({
-        'model': 'text-davinci-003',
-        'prompt': 'What is the capital of Spain?'
-    })
-}).then(response => response.json()).then(data => console.log(data))

d0ece4372ccd261fc877fb939a060aee4a2c3761
st ts <32254365+it-spectre-ru@users.noreply.github.com>
2023-05-15 11:10:25 +0500

    Update index.js
    del

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const setupTextarea = document.getElementById('setup-textarea')
 const setupInputContainer = document.getElementById('setup-input-container')
 const movieBossText = document.getElementById('movie-boss-text')
 
-const apiKey = "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"
+const apiKey = ""
 const url = 'https://api.openai.com/v1/completions'
 
 document.getElementById("send-btn").addEventListener("click", () => {

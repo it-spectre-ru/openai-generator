a9a8aa905ac9fb3687846e36f7d80cde94ae4294
stts <it.spectre.ru@gmail.com>
2023-05-15 23:00:55 +0500

    take out project

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import { process } from '/env'
 import { Configuration, OpenAIApi } from 'openai'
 
-
-const setupTextarea = document.getElementById('setup-textarea') 
+const setupTextarea = document.getElementById('setup-textarea')
 const setupInputContainer = document.getElementById('setup-input-container')
 const movieBossText = document.getElementById('movie-boss-text')
 
@@ -14,17 +13,16 @@ const openai = new OpenAIApi(configuration)
 
 document.getElementById("send-btn").addEventListener("click", () => {
   // if (setupTextarea.value) {
-    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
-    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
+  setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
+  movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
   // }
   fetchBotReply()
 })
 
-async function fetchBotReply(){
+async function fetchBotReply() {
   const response = await openai.createCompletion({
-      'model': 'text-davinci-003',
-      'prompt': 'Sound enthusiastic in five words or less.' 
+    'model': 'text-davinci-003',
+    'prompt': 'Sound enthusiastic in five words or less.'
   })
   movieBossText.innerText = response.data.choices[0].text.trim()
 }
-// [{text: " Excited, Enthusiastic, Driven!",

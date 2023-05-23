1b513bcf1f440f93dcfb859cc67f1f5a3b895afa
stts <it.spectre.ru@gmail.com>
2023-05-23 23:02:09 +0500

    let's go

--- index.js
+++ index.js
@@ -23,12 +23,9 @@ document.getElementById("send-btn").addEventListener("click", () => {
 async function fetchBotReply(outline) {
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `Generate a short message to enthusiastically say "${outline}" sounds interesting and that you need some minutes to think about it. Mention one aspect of the sentence.`,
-    // max_tokens: 60    
+    prompt: `Generate a short message to enthusiastically say "${outline}" sounds interesting and that you need some minutes to think about it. Mention one aspect of the sentence."`,
+    max_tokens: 60
   })
   movieBossText.innerText = response.data.choices[0].text.trim()
   console.log(response)
 } 
-
-// finish_reason: "stop"}], usage: {prompt_tokens: 44, completion_tokens: 59,
-// finish_reason: "length"}], usage: {prompt_tokens: 44, completion_tokens: 16,

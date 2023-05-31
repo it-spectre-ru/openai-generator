54d9e185934d48a51921c0cb79e4ce5550c6375d
stts <it.spectre.ru@gmail.com>
2023-05-31 22:02:08 +0500

    Reaching for the stars

--- index.js
+++ index.js
@@ -41,13 +41,18 @@ async function fetchBotReply(outline) {
     max_tokens: 60 
   })
   movieBossText.innerText = response.data.choices[0].text.trim()
-  console.log(response) 
 } 
 
 async function fetchSynopsis(outline) {
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline.
+/*
+Challenge:
+    1. Ask for actors names in brackets after each character. 
+       You could also suggest that OpenAI thinks of actors that would 
+       particularly suit the role. 
+*/
+    prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline. The synopsis should include actors names in brackets after each character. Choose actors that would be ideal for this role. 
     ###
     outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
     synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.  
@@ -60,6 +65,7 @@ async function fetchSynopsis(outline) {
   const synopsis = response.data.choices[0].text.trim()
   document.getElementById('output-text').innerText = synopsis
   fetchTitle(synopsis)
+  fetchStars(synopsis)
 }
 
 async function fetchTitle(synopsis) {
@@ -68,13 +74,26 @@ async function fetchTitle(synopsis) {
     prompt: `Generate a catchy movie title for this synopsis: ${synopsis}`,
     max_tokens: 25,
     temperature: 0.7
-  }) 
+  })
+  document.getElementById('output-title').innerText = response.data.choices[0].text.trim()
+}
+
+async function fetchStars(synopsis){
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
 /*
 Challenge:
-1. Write a prompt asking for a title based on a synopsis. 
-   You can specify that the title should be gripping, or flashy, 
-   or alluring if you would like. 
-2. Add 'model' and 'max_tokens' properties. 
+    1. Use OpenAI to extra the names in brackets from our synopsis.
 */
-  document.getElementById('output-title').innerText = response.data.choices[0].text.trim()
+    prompt: `Extract the names in brackets from the synopsis.
+    ###
+    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+    names: Tom Cruise, Val Kilmer, Kelly McGillis
+    ###
+    synopsis: ${synopsis}
+    names:   
+    `,
+    max_tokens: 30
+  })
+  document.getElementById('output-stars').innerText = response.data.choices[0].text.trim()
 }

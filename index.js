bc1ac3c117e788b29da0811b3114e617a52b0ea7
stts <it.spectre.ru@gmail.com>
2023-05-31 21:51:57 +0500

    Refactor fetchSynopsis

--- index.js
+++ index.js
@@ -1,52 +1,66 @@
-import { Configuration, OpenAIApi } from "openai";
-import { process } from './env';
+import { process } from '/env'
+import { Configuration, OpenAIApi } from 'openai'
+
+const setupInputContainer = document.getElementById('setup-input-container')
+const movieBossText = document.getElementById('movie-boss-text')
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
-document.getElementById("submit-btn").addEventListener("click", () => {
-  const productName = document.getElementById("name").value;
-  const productDesc = document.getElementById("desc").value;
-  const productTarget = document.getElementById("target").value;
-  getCopySuggestion(productName, productDesc, productTarget);
+  apiKey: process.env.OPENAI_API_KEY
 })
 
+const openai = new OpenAIApi(configuration)
 
-async function getCopySuggestion(productName, productDesc, productTarget) {
+document.getElementById("send-btn").addEventListener("click", () => {
+  const setupTextarea = document.getElementById('setup-textarea')
+  if (setupTextarea.value) {
+    const userInput = setupTextarea.value
+    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
+    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
+    fetchBotReply(userInput)
+    // fetchSynopsis(userInput)
+  }
+})
+
+async function fetchBotReply(outline) {
   const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    
-/*    
-Challenge:
-  1. Add a second example here. Be sure to make it similar in design to the first.
-  2. Remember to use separators 
-*/  
-    
-    
-    prompt: `Use a product name, a product description and a target market to create advertising copy for a product.
+    model: 'text-davinci-003',
+/*
+1. Refactor this prompt to use examples of an outline and an 
+   enthusiastic response. Be sure to keep the length of your 
+   examples reasonably short, say 20 words or so.
+*/
+    prompt: `Generate a short message to enthusiastically say an outline sounds interesting and that you need some minutes to think about it.
     ###
-    product name: Flask Tie
-    product description: A tie with a pouch to hold liquids and a straw to drink through
-    product traget market: office workers
-    advertising copy: Are you tired of having to worry about how much to drink throughout the day? With the Flask Tie, you can stay hydrated on-the-go! Our unique tie features a pouch that enables you to securely hold and sip your favorite drinks with the built-in straw! The water cooler is history! Long live Flask Tie!
+    outline: Two dogs fall in love and move to Hawaii to learn to surf.
+    message: I'll need to think about that. But your idea is amazing! I love the bit about Hawaii!
     ###
-    product name: SolarSwim
-    product description: Swimming costumes for all genders with solar cells to charge your devices while you sunbathe.
-    product traget market: Aimed at young adults
-    advertising copy: Don't miss a beat while you're having fun in the sun! SolarSwim is the perfect choice for the tech-savvy, on-the-go millennial. Our innovative swimming costumes come with integrated solar cells that allow you to charge and access your devices while you're at the beach or pool. Enjoy your summer break with SolarSwim!  
+    outline: A plane crashes in the jungle and the passengers have to walk 1000km to safety.
+    message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
     ###
-    product name: ${productName}
-    product description: ${productDesc}
-    product traget market: ${productTarget}
-    advertising copy: 
+    outline: A group of corrupt lawyers try to send an innocent woman to jail.
+    message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
+    ###
+    outline: ${outline}
+    message: 
     `,
-    max_tokens: 100,
-  });
-  document.getElementById('ad-output').insertAdjacentText('beforeend', response.data.choices[0].text.trim())
-  document.getElementById('ad-input').style.display = 'none'
-  document.getElementById('ad-output').style.display = 'block'
-  console.log(response)
+    max_tokens: 60 
+  })
+  movieBossText.innerText = response.data.choices[0].text.trim()
+  console.log(response) 
+} 
+
+async function fetchSynopsis(outline) {
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline
+    ###
+    outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
+    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+    ###
+    outline: ${outline}
+    synopsis: 
+    `,
+    max_tokens: 700
+  }) 
+  document.getElementById('output-text').innerText = response.data.choices[0].text.trim()
 }
